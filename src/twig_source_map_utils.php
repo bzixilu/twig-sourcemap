c65c7d31ab68ec3a10ba2ae7e4935f689a178eeb
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2018-09-14 17:57:12 +0300

    (minor) move to utils

--- src/twig_source_map_utils.php
+++ src/twig_source_map_utils.php
@@ -15,3 +15,38 @@ function isTwigTemplateName($name)
 {
     return endsWith($name, 'twig');
 }
+
+/**
+ * @param Twig_Environment $twig
+ * @param $name
+ * @throws Twig_Error_Syntax
+ * @throws Twig_Error_Loader
+ */
+function compileTemplateSource(Twig_Environment $twig, $name)
+{
+    $source = $twig->getLoader()->getSourceContext($name);
+    $content = $twig->compileSource($source);
+    file_put_contents(__DIR__ . '/../output/' . $name . '.php', $content . PHP_EOL);
+    file_put_contents(__DIR__ . '/../output/' . $name . '.php', '//# sourceMappingURL=' . $name . '.map', FILE_APPEND);
+}
+
+/**
+ * @param Twig_Environment $twig
+ * @param $name
+ * @return void
+ * @throws ReflectionException
+ * @throws Twig_Error_Loader
+ * @throws Twig_Error_Syntax
+ */
+function generateSourceMap(Twig_Environment $twig, $name)
+{
+    $template = $twig->createTemplate($name);
+    $path = $template->getSourceContext()->getPath();
+    $debug = $template->getDebugInfo();
+    $output = (new \ReflectionClass($template))->getFileName();
+    $map = \Kwf_SourceMaps_SourceMap::createEmptyMap($output);
+    foreach ($debug as $outputLine => $sourceLine) {
+        $map->addMapping($outputLine, 0, $sourceLine, 0, $path);
+    }
+    file_put_contents(__DIR__ . '/../output/' . $name . '.map', $map->getMapContents());
+}
